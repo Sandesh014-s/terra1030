50a12605e98dc33a4534a7d414183bedd454c7fb
Sandesh014-s <sandeshsable014@gmail.com>
2025-05-08 10:52:31 +0530

    Update main.tf

--- day-10-user_data/main.tf
+++ day-10-user_data/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   
 }
 resource "aws_instance" "css-test-user-data" {
-    ami = "ami-0f1dcc636b69a6438"
+    ami = "ami-0f88e80871fd81e91"
     instance_type = "t2.micro"
     user_data= file("test.sh")
     tags = {
